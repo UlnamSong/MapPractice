1c5c7d8dc52c1532758953d4577b81187f3fbefc
TaeinKim <gydect48@gmail.com>
2017-02-13 21:39:54 +0900

    Map Layout 생성 및 더미 위치 지정 후 동작 테스트

--- app/src/main/java/com/ulnamsong/mappractice/MainActivity.java
+++ app/src/main/java/com/ulnamsong/mappractice/MainActivity.java
@@ -4,11 +4,20 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ulnamsong.mappractice.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ActivityMainBinding mBinding;
+    static final LatLng SEOUL = new LatLng(37.56, 126.97);
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mBinding.textView.setText("Data Binding Good.");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap map) {
+        googleMap = map;
+
+        Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL)
+                .title("Seoul"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 }
